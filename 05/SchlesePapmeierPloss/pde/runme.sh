f4cb2fd7cf6e54016e95181b65f2f1f69abb77ad
Konstantin Schlese <konst@openmailbox.org>
2013-11-15 04:57:01 +0100

    Skript um teste auszuführen.

---
+++ 05/SchlesePapmeierPloss/pde/runme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 1 - presigion, 2 - iterations
+METHOD=1
+PRECISION=7e-08
+#PRECISION=100
+INTERLINES=10
+test1() \
+{
+	rm -rf ./partdiff-posix-*.txt
+	
+	REPORT_FILE="test1_state.txt"
+	rm -rf ${REPORT_FILE}
+	for NUM_THREADS in {1..12}
+	do
+		PARAMS="${NUM_THREADS} 2 ${INTERLINES} 2 ${METHOD} ${PRECISION}"
+		RESULT_FILE=partdiff-posix-${NUM_THREADS}.txt
+		echo "creating ${RESULT_FILE} and running ./partdiff-posix $PARAMS > ${RESULT_FILE}"
+		./partdiff-posix $PARAMS > ${RESULT_FILE}
+	done
+}
+
+test1
