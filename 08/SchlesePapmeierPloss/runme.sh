ce2106a247fdc4073b144b915d8466fed502f475
konstmonst <konst@openmailbox.org>
2013-12-27 11:33:39 +0100

    Testskripte hinzugefuegt

--- 08/SchlesePapmeierPloss/runme.sh
+++ 08/SchlesePapmeierPloss/runme.sh
@@ -3,16 +3,19 @@ set -e
 EXE=./partdiff-par
 #EXE=./partdiff-omp
 INTERLINES=100
-METHOD=1
+#METHOD=1
 FUNC=2
 COND=2
 PRES=100
 NUM_THREADS=2
-for i in {1 .. 12}
+rm -rf $EXE*.txt
+for METHOD in {1..2}
 do
-	CMDLINE="$EXE $NUM_THREADS $METHOD $INTERLINES  $FUNC $COND $PRES"
+    for i in {1..12}
+    do
+    	CMDLINE="mpirun -n $i $EXE $NUM_THREADS $METHOD $INTERLINES  $FUNC $COND $PRES"
 	echo doing $CMDLINE
-	echo $CMDLINE > $EXE-$NUM_THREADS-$METHOD-$INTERLINES-$FUNC-$COND-$PRES.txt 
-	mpirun -n $i $CMDLINE
+	echo $CMDLINE > $EXE-tasks$i-$NUM_THREADS-$METHOD-$INTERLINES-$FUNC-$COND-$PRES.txt 
+	$CMDLINE
+    done
 done
-
---
+++ 08/SchlesePapmeierPloss/submit.sh
@@ -0,0 +1,2 @@
+salloc -n1 srun -n1 -N1 --pty --preserve-env ./runme.sh
+
