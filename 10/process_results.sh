30d6ee51d10ae8870ee6e0ebb9225a2035613e17
Konstantin Schlese <konst@openmailbox.org>
2014-01-11 05:05:05 +0100

    Grafiken werden erzeugt.

--- 10/process_results.sh
+++ 10/process_results.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-results_dir = "/home/jcd/uni/hlr/10-Job-Scripte/out"
-outpu_dir = "/home/jcd/out"
+import os
+import glob
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Test:
-	def __init__(self, test_filename):
+	def __init__(self, job_name):
 		self.complete = False
-		self.test_filename = test_filename
-		tokens = test_filename.split("_")
+		self.job_name = job_name
+		tokens = self.job_name.split("_")
+		self.test_type = tokens[0].lower()
+		self.nodes = int(tokens[3])
+		self.tasks = int(tokens[4])
+		self.interlines = int(tokens[5])
+		N = self.interlines * 8 + 9
+		self.matrix_size = N * N
 		
 	def __str__(self):
 		if self.complete:
-			s = ("test_filename: " + self.test_filename + "\n"
+			s = ("job_name: " + self.job_name + "\n"
+				 "test_type: " + self.test_type + "\n"
+				 "nodes: " + str(self.nodes) + "\n"
+				 "tasks: " + str(self.tasks) + "\n"
+				 "interlines: " + str(self.interlines) + "\n"
+				 "matrix_size: " + str(self.matrix_size) + "\n"				 
 			     "num_tasks: " + str(self.num_tasks) + "\n"
 			     "method: " + self.method + "\n"
 			     "max_residuum: " + str(self.max_residuum) + "\n"
@@ -18,34 +33,39 @@ class Test:
 			     "time: " + str(self.time)
 			     )
 			return s
+			
+	def __lt__(self, other):
+		""" less than, used for sorting """
+		if self.complete == False:
+			return self
+		elif other.complete == False:
+			return other
 		else:
-			return "<incomplete test>"
-		
-		
-tests = []
+			return self.matrix_size < other.matrix_size
 
-import glob
-test_filenames = glob.glob(results_dir + "/*.out")
-for test_filename in test_filenames:
-	
-	with open(test_filename) as test_file:
-		
-		curr_test = Test(test_filename)
-		for line in test_file:
-			#print("line: " + line);
-			tokens = line.split()
-			if tokens[1] == "num_tasks":
-				curr_test.num_tasks = int(tokens[3])
-			elif tokens[1] == "method":
-				curr_test.method = tokens[3]
-			elif tokens[0] == "max":
-				curr_test.max_residuum = float(tokens[3][:-1])
-				curr_test.num_iterations = int(tokens[8])
-			elif tokens[0] == "time":
-				curr_test.time = float(tokens[2])
-				curr_test.complete = True;
-				tests.append(curr_test)
-				curr_test = Test(test_filename)
+def gen_tests_from_files(results_dir):
+	tests = []
+	job_output_filenames = glob.glob(results_dir + "/*.out")
+	for job_output_filename in job_output_filenames:	
+		with open(job_output_filename) as job_file:		
+			job_name = os.path.splitext(os.path.basename(job_output_filename))[0]
+			curr_test = Test(job_name)		
+			for line in job_file:
+				#print("line: " + line);
+				tokens = line.split()
+				if tokens[1] == "num_tasks":
+					curr_test.num_tasks = int(tokens[3])
+				elif tokens[1] == "method":
+					curr_test.method = tokens[3].lower()
+				elif tokens[0] == "max":
+					curr_test.max_residuum = float(tokens[3][:-1])
+					curr_test.num_iterations = int(tokens[8])
+				elif tokens[0] == "time":
+					curr_test.time = float(tokens[2])
+					curr_test.complete = True;
+					tests.append(curr_test)
+					curr_test = Test(job_name)
+	return tests
 
 
 def mean(tests):
@@ -54,13 +74,10 @@ def mean(tests):
 	for test in tests:
 		max_residuum = max_residuum + test.max_residuum
 		time = time + test.time
-	result_test = Test(tests[0].test_filename)
+	#result_test = copy.copy(tests[0])
+	result_test = copy.deepcopy(tests[0])
 	result_test.max_residuum = max_residuum / len(tests)
 	result_test.time = time / len(tests)
-	result_test.num_tasks = tests[0].num_tasks
-	result_test.method = tests[0].method
-	result_test.num_iterations = tests[0].num_iterations
-	result_test.complete = True
 	return result_test
 
 def smooth_tests(tests_in, tests_per_job):
@@ -71,16 +88,69 @@ def smooth_tests(tests_in, tests_per_job):
 		batch.append(test)
 		i = i + 1
 		if i == tests_per_job:
-			i = 0
-			tests_out.append(mean(batch))
-			to_smooth = []
+			i = 0			
+			m = mean(batch)
+			print("appending" + str(m))
+			tests_out.append(m)
+			batch = []
 	return tests_out				
 
-tests = smooth_tests(tests, 3)
+def plot_weak_tests_matrix_size(tests, name):
+	if not tests:
+		raise BaseException("no tests passed to plot_tests")
+	norm_matrix_size = tests[0].matrix_size
+	norm_time = tests[0].time
+	x = []
+	y = []
+	for test in tests:
+		#x.append(test.matrix_size / norm_matrix_size)
+		x.append(test.matrix_size) # matrix size
+		y.append(norm_time / test.time) # speedup
+	for i in range(0, len(x)):
+		print("(" + str(x[i]) + ", " + str(y[i]) + ")")
+	plt.plot(x, y, label=name)
+	#plt.show()
+	
 
+results_dir = "/home/jcd/uni/hlr/10-Job-Scripte/out"
+outpu_dir = "/home/jcd/out"
+tests = gen_tests_from_files(results_dir)
+tests = smooth_tests(tests, 3)
+print(str(len(tests)))
+weak_scaling_ja = []
+weak_scaling_ga = []
+strong_scaling = []
+comm = []
 for test in tests:	
-	print(test)
-	print("------------------\n")
-	
-	
+	#print(test)
+	#print("------------------\n")
+	if test.test_type == "weak":
+		if test.method == "jacobi":
+			weak_scaling_ja.append(test)
+		else:
+			weak_scaling_ga.append(test)
+	elif test.test_type == "strong":
+		strong_scaling.append(test)
+	elif test.test_type == "communication":
+		comm.append(test)
+	else:
+		raise BaseException("unknown test type: " + test.test_type)
+
+weak_scaling_ja.sort()
+weak_scaling_ga.sort()
+strong_scaling.sort()
+comm.sort()
+
+plt.xlabel("matrix size (nr. of elements)")
+plt.ylabel("speedup")
+plot_weak_tests_matrix_size(weak_scaling_ja, "Weak Scaling with Jacobi")
+plot_weak_tests_matrix_size(weak_scaling_ga, "Weak Scaling with Gauss")
+plt.legend()
+#plt.savefig("weak_matrix_size.png")
+plt.show()
+plt.clf()
+#plot_tests(strong_scaling)
+#plot_tests(comm)
+
+
 
